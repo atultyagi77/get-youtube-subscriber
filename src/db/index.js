dbd7de21ed3010609968e01ca69df610023eb096
Atul Kumar <tyagiatul23799@gmail.com>
2024-04-06 23:50:23 +0530

    connect to Mongo Atlas

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import express from 'express';
+
+
+const app= express()
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB host : ${connectionInstance.connection.host}`);
+        app.listen(process.env.PORT, () => {
+            console.log(`Application is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("MongoDB connection Error: ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
