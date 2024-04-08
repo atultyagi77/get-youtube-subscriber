8bc6ae6431ed29439cb7e29a3184632dfe780b88
Atul Kumar <tyagiatul23799@gmail.com>
2024-04-08 16:49:19 +0530

    removed dotenv file from git

--- src/db/index.js
+++ src/db/index.js
@@ -3,12 +3,16 @@ import { DB_NAME } from "../constants.js";
 import express from 'express';
 
 
-const app= express()
+const app = express()
 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB host : ${connectionInstance.connection.host}`);
+        app.on("error", () => {
+            console.log("Not Able to talk to DB: ", error);
+            throw error
+        })
         app.listen(process.env.PORT, () => {
             console.log(`Application is listening on port ${process.env.PORT}`);
         })
